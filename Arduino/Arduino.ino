91ef22c615474a4db4e8a2632439c713fc3cb9fb
Alireza-2001 <Alireza1379.sharifi@gmail.com>
2023-11-21 23:31:06 +0330

    Arduino coding

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ArduinoJson.h>
 
+const unsigned long serial_monitor_baudrate = 115200;
+const unsigned long raspi_serial_baudrate = 115200;
+
+
+DynamicJsonDocument joystick(1024);
+DynamicJsonDocument setting(1024);
+
+
+void setup()
+{
+  Serial.begin(serial_monitor_baudrate);
+  Serial1.begin(raspi_serial_baudrate);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void get_serial_data()
+{
+  if (Serial1.available() > 0)
+  {
+    String tmp = Serial1.readStringUntil('>');
+    if (tmp.length() == 59)
+    {
+      deserializeJson(joystick, tmp);
 
+    }
+    else if (tmp.length() == 12)
+    {
+      deserializeJson(setting, tmp);
+
+    }
+  }
+  else
+  {
+
+  }
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
 }
