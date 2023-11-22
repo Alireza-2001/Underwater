37b44dc864e6d8e8d9e597acecde038b5b82979d
Alireza-2001 <Alireza1379.sharifi@gmail.com>
2023-11-22 21:11:35 +0330

    create GUI for raspi App

--- Raspi/app.py
+++ Raspi/app.py
@@ -1,5 +1,27 @@
-def main():
-    print("Hello world.")
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from mainwindow import Ui_mainwindow
 
-if __name__ == "__main__":
-    main()
+
+class MainWindowClass(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_mainwindow()       
+        self.ui.setupUi(self)
+        self.set_style()
+        self.setup_connection()
+        
+
+    def set_style(self):
+        self.ui.groupBox_4.setStyleSheet("QGroupBox { border: 1px solid red;}")
+    
+    def setup_connection(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindowClass()
+    main_window.show()
+    sys.exit(app.exec())
