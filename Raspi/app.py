dccfb1b76abd1df5b08b97c2691ae13ed29c51c4
hosein <119526872+sedhoseinart@users.noreply.github.com>
2023-12-02 20:10:44 +0330

    complate raspi programm

--- Raspi/app.py
+++ Raspi/app.py
@@ -1,72 +1,33 @@
-import sys
 import socket, json, datetime, os, serial, cv2
 from flask import Flask, render_template, Response
 from time import time, sleep
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QWidget
-from mainwindow import Ui_mainwindow
+from threading import Thread, main_thread
 
+app=Flask(__name__)
 
 try:
-    camera = cv2.VideoCapture(0)
+    camera_1 = cv2.VideoCapture(0)
+    camera_2 = cv2.VideoCapture(1)
     ser = serial.Serial("/dev/ttyS0", baudrate = 115200, timeout=1)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except Exception as e:
-     print(e)
+    print(e)
+
+raspi_IP = ''
+data_port = 8000
+laptop_IP = ''
+
+def socket_config():
+    global raspi_IP
+    global data_port
+    try:
+        s.connect((raspi_IP, data_port))
+    except Exception as e:
+        print(e)
 
 
-rsspi_ip = ""
-laptop_ip = ""
-
-
-class ControllerThreadClass(QtCore.QThread):	
-    data_signal = QtCore.pyqtSignal(list)
-    def __init__(self,controller, parent=None):
-        super(ControllerThreadClass, self).__init__(parent)
-        self.is_running = True
-                
-    def run(self):
-        while True:
-            data = ser.readline()
-            if data:
-                data = data.decode("utf-8").split(",")
-                if len(data) == 13:
-
-                    print(data)
-                    self.data_signal.emit(data)
-
-    def stop(self):
-        self.is_running = False
-        data = {'status' : True, 'message' : 'Stopping controller thread...', 'data' : ''}
-        self.message_signal.emit(data)
-        self.terminate()
-
-
-class MainWindowClass(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.ui = Ui_mainwindow()
-        self.ui.setupUi(self)
-        self.set_style()
-        self.setup_connection()
-        
-
-    def set_style(self):
-        self.ui.gp1.setStyleSheet("QGroupBox { border: 1px solid blue;}")
-        self.ui.gp2.setStyleSheet("QGroupBox { border: 1px solid blue;}")
-        self.ui.gp3.setStyleSheet("QGroupBox { border: 1px solid blue;}")
-        self.ui.gp4.setStyleSheet("QGroupBox { border: 1px solid blue;}")
-
-    
-    def setup_connection(self):
-        self.ui.btn_connect.clicked.connect(self.socket_connect)
-
-    def socket_connect(self):
-        print(self.ui.txt_raspi_ip.text())
+def mian():
+    pass
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main_window = MainWindowClass()
-    main_window.show()
-    sys.exit(app.exec())
+    main()
