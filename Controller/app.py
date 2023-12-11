50fb624f6aa20a5ddaef0bdb5bb4ff8d5cb49cdb
alireza-2001 <alireza1379.sharifi@gmail.com>
2023-12-11 17:12:16 +0330

    fix

--- Controller/app.py
+++ Controller/app.py
@@ -18,8 +18,8 @@ LED_front = 0
 LED_second = 0
 
 data_url = 'http://192.168.0.115:4000/fanoos/v1.0/data'
-camera_1_url = 'http://192.168.0.115:4000//video_feed/1'
-camera_2_url = 'http://192.168.0.115:4000//video_feed/2'
+camera_1_url = 'http://192.168.0.115:4000//video_feed_1'
+camera_2_url = 'http://192.168.0.115:4000//video_feed_2'
 
 threads = {}
 controller = Controller()
